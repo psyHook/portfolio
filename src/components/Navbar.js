706b37373e8f9b839101a869c87946d588cd035b
Juan Galarce <j6alarce@gmail.com>
2021-02-05 19:33:25 -0300

    Experience y ajustes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -43,7 +43,7 @@ const Navbar = () => {
               </li>
               <li className="nav-item">
                 <a className="nav-link" href="#!">
-                  How Work
+                  Experience
                 </a>
               </li>
               <li className="nav-item">
