7b7c4568a2ad1cff58d5a9686938ed0b5feecb24
Juan Galarce <j6alarce@gmail.com>
2021-02-11 23:49:41 -0300

    Estructura del componente contact

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -10,6 +10,52 @@ const Contacts = () => {
           you soon as possible.
         </p>
       </div>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6 col-xs-12">
+            <input
+              id="name"
+              type="text"
+              className="form-control"
+              placeholder="Name"
+              name="name"
+            />
+            <input
+              type="text"
+              id="phone"
+              className="form-control"
+              placeholder="Phone Number"
+              name="phone"
+            />
+            <input
+              id="email"
+              type="email"
+              className="form-control"
+              placeholder="Email"
+              name="email"
+            />
+            <input
+              id="subject"
+              type="text"
+              className="form-control"
+              placeholder="Subject"
+              name="subject"
+            />
+          </div>
+          <div className="col-md-6 col-xs-12">
+            <textarea
+              id="description"
+              type="text"
+              className="form-control"
+              placeholder="Email"
+              name="email"
+            ></textarea>
+            <button className="btn-main-offer contact-btn" type="submit">
+              Contact me
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
