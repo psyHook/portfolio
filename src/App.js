1da6f5bd0dbb3bb0a3e27147aaee81c3a136578c
Juan Galarce <j6alarce@gmail.com>
2021-02-11 23:37:13 -0300

    Create Contact component

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Services from "./components/Services";
 import Experience from "./components/Experience";
 import Portfolio from "./components/Portfolio";
 import Testimonials from "./components/Testimonials";
+import Contacts from "./components/Contacts";
 
 
 import Logo from "./assets/img/logo.png";
@@ -77,6 +78,7 @@ function App() {
       <Experience />
       <Portfolio />
       <Testimonials />
+      <Contacts />
     </div>
   );
 }
---
+++ src/components/Contacts.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Contacts = () => {
+  return (
+    <div className="contacts">
+      <h1>contact me</h1>
+      <p>
+        Please fill out the form and describe tu project need and I'll contact
+        you soon as possible.
+      </p>
+    </div>
+  );
+};
+
+export default Contacts;
