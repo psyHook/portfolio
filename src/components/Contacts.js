61e7ae6a1e7fe63a9e1cfb7ee97ba3873f0fd822
Juan Galarce <j6alarce@gmail.com>
2021-02-12 20:54:35 -0300

    Se instala emailjs para el formulario de contacto y se crea la funcion para enviar mail

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,6 +1,24 @@
 import React from "react";
+import emailjs from "emailjs-com";
 
 const Contacts = () => {
+  const serviceID = "service_ID";
+  const templateID = "template_ID";
+  const userID = "user_DhzYeUnQyYTcFRK5fnKie";
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm(serviceID, templateID, e.target, userID).then(
+      (result) => {
+        console.log(result.text);
+      },
+      (error) => {
+        console.log(error.text);
+      }
+    );
+  };
+
   return (
     <div className="contacts">
       <div className="text-center">
@@ -11,68 +29,71 @@ const Contacts = () => {
         </p>
       </div>
       <div className="container">
-        <div className="row">
-          <div className="col-md-6 col-xs-12">
-            <div className="text-center">
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Name"
-                name="name"
-              />
-              <div className="line"></div>
+        <form onSubmit={sendEmail}>
+          <div className="row">
+            <div className="col-md-6 col-xs-12">
+              <div className="text-center">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Name"
+                  name="name"
+                  autocomplete="off"
+                />
+                <div className="line"></div>
+              </div>
+              <span className="error-message"></span>
+              <div className="text-center">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Phone Number"
+                  name="phone"
+                  autocomplete="off"
+                />
+                <div className="line"></div>
+              </div>
+              <span className="error-message"></span>
+              <div className="text-center">
+                <input
+                  type="email"
+                  className="form-control"
+                  placeholder="Email"
+                  name="email"
+                  autocomplete="off"
+                />
+                <div className="line"></div>
+              </div>
+              <span className="error-message"></span>
+              <div className="text-center">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Subject"
+                  name="subject"
+                  autocomplete="off"
+                />
+                <div className="line"></div>
+              </div>
+              <span className="error-message"></span>
             </div>
-            <span className="error-message">
-            </span>
-            <div className="text-center">
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Phone Number"
-                name="phone"
-              />
-              <div className="line"></div>
+            <div className="col-md-6 col-xs-12">
+              <div className="text-center">
+                <textarea
+                  type="text"
+                  className="form-control"
+                  placeholder="Please describe shortly you project..."
+                  name="description"
+                  autocomplete="off"
+                ></textarea>
+                <div className="line"></div>
+              </div>
+              <button className="btn-main-offer contact-btn" type="submit">
+                Contact me
+              </button>
             </div>
-            <span className="error-message">
-            </span>
-            <div className="text-center">
-              <input
-                type="email"
-                className="form-control"
-                placeholder="Email"
-                name="email"
-              />
-              <div className="line"></div>
-            </div>
-            <span className="error-message">
-            </span>
-            <div className="text-center">
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Subject"
-                name="subject"
-              />
-              <div className="line"></div>
-            </div>
-            <span className="error-message">
-            </span>
           </div>
-          <div className="col-md-6 col-xs-12">
-            <div className="text-center">
-              <textarea
-                type="text"
-                className="form-control"
-                placeholder="Please describe shortly you project..."
-                name="description"
-              ></textarea>
-              <div className="line"></div>
-            </div>
-            <button className="btn-main-offer contact-btn" type="submit">
-              Contact me
-            </button>
-          </div>
-        </div>
+        </form>
       </div>
     </div>
   );
