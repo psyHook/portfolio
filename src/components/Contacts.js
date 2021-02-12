ffe5faffdeffc7df79627867f2e108813910e468
Juan Galarce <j6alarce@gmail.com>
2021-02-11 23:41:55 -0300

    se agrega el css para el encabezado del formulario de contact

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -3,11 +3,13 @@ import React from "react";
 const Contacts = () => {
   return (
     <div className="contacts">
-      <h1>contact me</h1>
-      <p>
-        Please fill out the form and describe tu project need and I'll contact
-        you soon as possible.
-      </p>
+      <div className="text-center">
+        <h1>contact me</h1>
+        <p>
+          Please fill out the form and describe tu project need and I'll contact
+          you soon as possible.
+        </p>
+      </div>
     </div>
   );
 };
