48fe9c2d7fe4bff09e64a3da9682370d4991e9de
Juan Galarce <j6alarce@gmail.com>
2021-02-15 11:33:39 -0300

    Contact component completo y funcionando

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,22 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import emailjs from "emailjs-com";
+import { useForm } from "react-hook-form";
 
 const Contacts = () => {
+  const [successMessage, setSuccessMessage] = useState("");
+
+  const { register, handleSubmit, errors } = useForm();
+
   const serviceID = "service_ID";
   const templateID = "template_ID";
   const userID = "user_DhzYeUnQyYTcFRK5fnKie";
 
-  const sendEmail = (e) => {
-    e.preventDefault();
-
-    emailjs.sendForm(serviceID, templateID, e.target, userID).then(
-      (result) => {
-        console.log(result.text);
+  const onSubmit = (data, r) => {
+    sendEmail(
+      serviceID,
+      templateID,
+      {
+        name: data.name,
+        phone: data.phone,
+        email: data.email,
+        subject: data.subject,
+        description: data.description,
       },
-      (error) => {
-        console.log(error.text);
-      }
+      userID
     );
+    r.target.reset();
+  };
+
+  const sendEmail = (serviceID, templateID, variables, userID) => {
+    emailjs
+      .send(serviceID, templateID, variables, userID)
+      .then(() => {
+        setSuccessMessage(
+          "Form sent successfyully! I'll contact you as soon as possuble."
+        );
+      })
+      .catch((err) => console.error(`Something went wrong ${err}`));
   };
 
   return (
@@ -27,9 +46,10 @@ const Contacts = () => {
           Please fill out the form and describe tu project need and I'll contact
           you soon as possible.
         </p>
+        <span className="success-message">{successMessage}</span>
       </div>
       <div className="container">
-        <form onSubmit={sendEmail}>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className="row">
             <div className="col-md-6 col-xs-12">
               <div className="text-center">
@@ -39,10 +59,20 @@ const Contacts = () => {
                   placeholder="Name"
                   name="name"
                   autocomplete="off"
+                  ref={register({
+                    required: "Please enter your name",
+                    maxLength: {
+                      value: 20,
+                      message:
+                        "Please enter a name with fewer than 20 characters",
+                    },
+                  })}
                 />
                 <div className="line"></div>
               </div>
-              <span className="error-message"></span>
+              <span className="error-message">
+                {errors.name && errors.name.message}
+              </span>
               <div className="text-center">
                 <input
                   type="text"
@@ -50,10 +80,15 @@ const Contacts = () => {
                   placeholder="Phone Number"
                   name="phone"
                   autocomplete="off"
+                  ref={register({
+                    required: "Please add your phone number",
+                  })}
                 />
                 <div className="line"></div>
               </div>
-              <span className="error-message"></span>
+              <span className="error-message">
+                {errors.phone && errors.phone.message}
+              </span>
               <div className="text-center">
                 <input
                   type="email"
@@ -61,10 +96,19 @@ const Contacts = () => {
                   placeholder="Email"
                   name="email"
                   autocomplete="off"
+                  ref={register({
+                    required: "Please provide you email",
+                    pattern: {
+                      value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                      message: "Invalid email",
+                    },
+                  })}
                 />
                 <div className="line"></div>
               </div>
-              <span className="error-message"></span>
+              <span className="error-message">
+                {errors.email && errors.email.message}
+              </span>
               <div className="text-center">
                 <input
                   type="text"
@@ -72,10 +116,15 @@ const Contacts = () => {
                   placeholder="Subject"
                   name="subject"
                   autocomplete="off"
+                  ref={register({
+                    required: "OOPS, you forget to add the subject.",
+                  })}
                 />
                 <div className="line"></div>
               </div>
-              <span className="error-message"></span>
+              <span className="error-message">
+                {errors.subject && errors.subject.message}
+              </span>
             </div>
             <div className="col-md-6 col-xs-12">
               <div className="text-center">
@@ -85,9 +134,15 @@ const Contacts = () => {
                   placeholder="Please describe shortly you project..."
                   name="description"
                   autocomplete="off"
+                  ref={register({
+                    required: "Please describe shortly your project needs...",
+                  })}
                 ></textarea>
                 <div className="line"></div>
               </div>
+              <span className="error-message">
+                {errors.description && errors.description.message}
+              </span>
               <button className="btn-main-offer contact-btn" type="submit">
                 Contact me
               </button>
